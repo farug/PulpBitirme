4c12a5f73c2c7698fcdf26da764eece709d213a6
omer sert <omer.sert@tubitak.gov.tr>
2024-04-30 13:26:54 +0300

    ansible script added for toolchain and overall installation bash script is written

---
+++ Scripts/BASH/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sshpass -p 1111 ssh pulp@192.168.1.13 'bash -s' < install_riscv_toolchain.sh > toolchain.log
+exit
+sshpass -p 1111 ssh pulp@192.168.1.13 'bash -s' < PULPissimo.sh > PULP.log
+exit
+#sshpass -p 1111 ssh pulp@192.168.1.13 'bash s' <PULP_Simulation.sh
+#exit
